5c066305f29d94a791a64657dc8577760a17bfb0
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2022-07-15 19:14:23 +0500

    bug fixed

--- src/modules/products/model.js
+++ src/modules/products/model.js
@@ -1,7 +1,7 @@
 import { fetchData } from '../../utils/postgres.js'
 
 const notebooks = `
-    Select * FROM products p inner join categories c on c.category_name = 'notebooks' where p.product_category_id = c.category_id;
+    Select p.product_name, p.product_price, p.product_desc, p.product_img_link, p.product_category_id FROM products p inner join categories c on c.category_name = 'notebooks' where p.product_category_id = c.category_id;
 `
 
 //FUNCTIONS
